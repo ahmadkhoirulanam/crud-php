3feb7a39d476531f34e612461e58b92120de6423
ahmadkhoirulanam <kanam5999@gmail.com>
2024-04-17 13:19:19 +0700

    login

--- koneksi.php
+++ koneksi.php
@@ -2,10 +2,25 @@
 	$servername = "localhost";
 	$username = "root"; // Ganti dengan username MySQL Anda
 	$password = ""; // Ganti dengan password MySQL Anda
-	$database = "query"; // Ganti dengan nama database Anda
+	$database = "informatika"; // Ganti dengan nama database Anda
 	$conn = new mysqli($servername, $username, $password, $database);
 	// Memeriksa koneksi
 	if ($conn->connect_error) {
 	    die("Koneksi gagal: " . $conn->connect_error);
 	}
+
+	function baseurl($link=''){
+        $baseurl="http://localhost/crud/";
+        
+        if(!empty($link)){
+            $url=$baseurl.$link;
+        }else{
+            $url=$baseurl;
+        }
+        return $url;
+    }
+    function redirectto($url="",$second=0){
+        $redirecto = '<meta http-equiv="refresh" content="'.$second.';url='.baseurl($url).'">';
+        echo $redirecto;die;
+    }
 ?>
